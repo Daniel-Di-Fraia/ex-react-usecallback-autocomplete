d3369a0800ca45bf5d07686ed2252e0642eb22c8
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2026-01-02 16:27:52 +0100

    finalizzazione milestone 2 Implementare il Debounce per Ottimizzare la Ricerca)

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,22 +1,43 @@
-import { useState, useEffect } from 'react';
+function debounce(callback, delay) {
+    let timer;
+    return (value) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            callback(value)
+        }, delay);
+    };
+}
+
+
+import { useState, useEffect, useCallback } from 'react';
 
 function InputSearch() {
     const [search, setSearch] = useState("");
     const [suggestions, setSuggestions] = useState([]);
 
-    useEffect(() => {
-        if (search.length > 0) {
-            fetch(`http://localhost:3333/products?search=${search}`)
+   const handleFetch = useCallback(
+    debounce((query) => {
+        if (query.length > 0) {
+            fetch(`http://localhost:3333/products?search=${query}`)
                 .then(res => res.json())
-                .then(data => setSuggestions(data))
+                .then(data => {
+                    setSuggestions(data);
+                    console.log(query); 
+                })
                 .catch(error => console.error(error));
         } else {
             setSuggestions([]);
         }
+    }, 1000),
+    [] 
+);
+
+    useEffect(() => {
+        handleFetch(search)
     }, [search]);
 
     const handleSelect = (name) => {
-        setSearch(name);    
+        setSearch(name);
         setSuggestions([]);
     };
 
