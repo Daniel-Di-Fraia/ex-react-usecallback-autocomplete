0169d6b2ecb6cb15426f1f5e086559c94c74af35
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2026-01-02 12:53:21 +0100

    finalizzazione milestone 1 (Creare un campo di ricerca e mostrare la lista dei suggerimenti)

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,18 +1,46 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 
 function InputSearch() {
-
     const [search, setSearch] = useState("");
     const [suggestions, setSuggestions] = useState([]);
 
-    
-  
+    useEffect(() => {
+        if (search.length > 0) {
+            fetch(`http://localhost:3333/products?search=${search}`)
+                .then(res => res.json())
+                .then(data => setSuggestions(data))
+                .catch(error => console.error(error));
+        } else {
+            setSuggestions([]);
+        }
+    }, [search]);
 
-  return (
-    <>
-     
-    </>
-  )
+    const handleSelect = (name) => {
+        setSearch(name);    
+        setSuggestions([]);
+    };
+
+    return (
+        <div className="row">
+            <h1>Cerca il tuo prodotto!</h1>
+            <input
+                type='text'
+                placeholder='Cerca prodotto...'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            {suggestions.length > 0 && (
+                <ul className="prod-list">
+                    <li id="title">SUGGERIMENTI</li>
+                    {suggestions.map((item) => (
+                        <li key={item.id} onClick={() => handleSelect(item.name)}>
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
 
-export default InputSearch
+export default InputSearch;
