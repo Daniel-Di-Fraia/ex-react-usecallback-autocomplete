efecdbda145e4181962e6f94dce8313d8c26b382
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2026-01-02 12:13:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import InputSearch from './components/InputSearch'
+
+function App() {
+  
+
+  return (
+    <>
+     <InputSearch />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/InputSearch.jsx
@@ -0,0 +1,18 @@
+import {useState, useEffect} from 'react'
+
+function InputSearch() {
+
+    const [search, setSearch] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+
+    
+  
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default InputSearch
